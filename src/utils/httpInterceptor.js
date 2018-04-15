77e6d864738d8b1c7061b5f56f883f31f21fd679
Sanjay <sanjayshiwakoti@gmail.com>
2018-04-15 18:14:11 +0545

    Add environment.

--- src/utils/httpInterceptor.js
+++ src/utils/httpInterceptor.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-axios.interceptors.request.use(function (config) {
-    config.headers = {'x-request-id': process.env.MSA_TRANSACTION_KEY};    
+axios.interceptors.request.use(
+  function(config) {
+    config.headers = { 'x-request-id': process.env.MSA_COMMON_AUTH_KEY };
+
     return config;
-  }, function (error) {
+  },
+  function(error) {
     return Promise.reject(error);
-  });
+  }
+);
