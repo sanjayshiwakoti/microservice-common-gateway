4b4c7f090758d6bda04c79f4c41a7cd53f7bce6f
Achyut Pokhrel <achyut.pkl@gmail.com>
2018-05-06 13:32:46 +0545

    Handle exception

--- src/controllers/gatewayController.js
+++ src/controllers/gatewayController.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { Router } from 'express';
-import HttpStatus from 'http-status-codes';
 
 const router = Router();
 
@@ -10,22 +9,40 @@ const router = Router();
 router.all('*', (req, res, next) => {
   switch (req.method) {
     case 'GET':
-      axios.get(`${req.clientGatewayURL}${req.originalUrl}`).then(response => res.json(response.data));
+      axios
+        .get(`${req.clientGatewayURL}${req.originalUrl}`)
+        .then(response => res.json(response.data))
+        .catch(e => res.status(e.response.status).json(e.response.data));
       break;
     case 'POST':
-      axios.post(`${req.clientGatewayURL}${req.originalUrl}`, req.body).then(response => res.json(response.data));
+      axios
+        .post(`${req.clientGatewayURL}${req.originalUrl}`, req.body)
+        .then(response => res.json(response.data))
+        .catch(e => res.status(e.response.status).json(e.response.data));
       break;
     case 'PUT':
-      axios.put(`${req.clientGatewayURL}${req.originalUrl}`, req.body).then(response => res.json(response.data));
+      axios
+        .put(`${req.clientGatewayURL}${req.originalUrl}`, req.body)
+        .then(response => res.json(response.data))
+        .catch(e => res.status(e.response.status).json(e.response.data));
       break;
     case 'PATCH':
-      axios.patch(`${req.clientGatewayURL}${req.originalUrl}`, req.body).then(response => res.json(response.data));
+      axios
+        .patch(`${req.clientGatewayURL}${req.originalUrl}`, req.body)
+        .then(response => res.json(response.data))
+        .catch(e => res.status(e.response.status).json(e.response.data));
       break;
     case 'DELETE':
-      axios.delete(`${req.clientGatewayURL}${req.originalUrl}`, req.body).then(response => res.json(response.data));
+      axios
+        .delete(`${req.clientGatewayURL}${req.originalUrl}`, req.body)
+        .then(response => res.json(response.data))
+        .catch(e => res.status(e.response.status).json(e.response.data));
       break;
     default:
-      axios.get(`${req.clientGatewayURL}${req.originalUrl}`).then(response => res.json(response.data));
+      axios
+        .get(`${req.clientGatewayURL}${req.originalUrl}`)
+        .then(response => res.json(response.data))
+        .catch(e => res.status(e.response.status).json(e.response.data));
       break;
   }
 });
